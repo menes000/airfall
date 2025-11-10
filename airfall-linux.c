3424ec9ace14e4f922138817768d021ac30e8d56
menes000 <menes_arslan@hotmail.com>
2025-11-10 14:49:11 +0300

    ktuphaen tnaimlari

---
+++ airfall-linux.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>i
+
+void print_my_ip(){
+
+
+
+}
