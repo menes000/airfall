61f8601eabfb179bd907cd8eb3d486b0c0e55508
menes000 <menes_arslan@hotmail.com>
2025-11-15 02:31:59 +0300

    ip gosterme eklendi

--- airfall-linux.c
+++ airfall-linux.c
@@ -6,8 +6,32 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-char* get_my_ip(){
+char* get_lan_ip();
 
+int main(){
+	get_lan_ip();	
+	
+}
 
+char* get_lan_ip(){
+	struct ifaddrs *ifaddr, *ifa;
+	getifaddrs(&ifaddr);
+        char ip[INET_ADDRSTRLEN];
+
+    	for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL) continue;
+
+        // Sadece IPv4 adresleri
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+
+            // Sadece WiFi interface'lerini kabul et (wlan0 veya wlp* gibi)
+            if (ifa->ifa_name[0] == 'w' && ifa->ifa_name[1] == 'l') {
+                struct sockaddr_in *in = (struct sockaddr_in*)ifa->ifa_addr;
+                inet_ntop(AF_INET, &in->sin_addr, ip, INET_ADDRSTRLEN);
+
+                printf("IP (%s): %s\n", ifa->ifa_name, ip);
+            }
+        }
+    }
 
 }
